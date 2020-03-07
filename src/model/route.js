d94aec56700a73af7b7cadcab8e0bc30e949af1e
isafdezpe <isafdezpe98@gmail.com>
2020-03-07 21:37:16 +0100

    checking route distance and slope not undefined

--- src/model/route.js
+++ src/model/route.js
@@ -1,6 +1,7 @@
 // @flow
 import Comment from './comment';
 import Milestone from './milestone';
+import { MilestoneService } from '@services';
 
 export default class Route {
   // TODO 
@@ -8,6 +9,7 @@ export default class Route {
 
   messages = [];
   milestones = [];
+  milestonesObject = [];
 
   /**
    * 
@@ -51,6 +53,22 @@ export default class Route {
 
   getIdentifier() {
     return `${this.name}_${this.createdBy}`;
+  }
+
+  getObjectsMilestones = async () => {
+    let distance = 0;
+    let slope = 0;
+    for (let i = 0; i < this.milestones.length; i++) {
+      this.milestonesObject.push(await MilestoneService.get(this.milestones[i]));
+      if (!this.distance)
+        distance += this.milestonesObject[i].distance;
+      if (!this.slope) {
+        if (i > 0) 
+          slope = Math.abs(this.milestonesObject[i] - this.milestonesObject[i-1]);
+      }
+    }
+    this.distance = distance;
+    this.slope = slope;
   }
 
 }
