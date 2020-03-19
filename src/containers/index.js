9bddeb680fe8af2125df587955cf1acbd84593ba
Javier Martínez Álvarez <javiermartinezalvarez98@gmail.com>
2020-03-19 17:42:05 +0100

    Remove useless code

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,11 +3,8 @@ import Register, { RegistrationSuccess } from './Register';
 import PageNotFound from './PageNotFound';
 import Welcome from './Welcome';
 import Profile from './Profile';
-import TextEditor from './TextEditor';
 import AddRoute from './AddRoute';
 import AddMilestone from './AddMilestone';
-import { GamePage, GameList } from './TicTacToe';
-import { FormModelConverter, FormModelRenderer } from './FormModel';
 import GoogleMap from './GoogleMap';
 
 export {
@@ -17,11 +14,6 @@ export {
   PageNotFound,
   Welcome,
   Profile,
-  GamePage,
-  GameList,
-  FormModelConverter,
-  FormModelRenderer,
-  TextEditor,
   GoogleMap,
   AddRoute,
   AddMilestone
