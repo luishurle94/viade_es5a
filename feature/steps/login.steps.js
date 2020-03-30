c1c844480e5e1fcc22b9545f9940e3023b2c511f
Adri-San <adrianvadrianv@outlook.com>
2020-03-31 00:53:36 +0200

    Test Feature - Se consigue abrir el navegador y direccionar a una url

--- feature/steps/login.steps.js
+++ feature/steps/login.steps.js
@@ -2,39 +2,67 @@ import 'jest'
 import { defineFeature, loadFeature } from 'jest-cucumber';
 
 const feature = loadFeature('./feature/features/login.feature');
-const INPUT_SELECTOR = "section input";
-const ENTER_EVENT = "Enter";
+const puppeteer = require('puppeteer');
+
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
 
 defineFeature(feature, test => {
 
   beforeEach(async () => {
-    await page.goto("http://localhost:3000/#/login");
-    await page.evaluate(() => {
-      localStorage.clear();
+
+    const browser = await puppeteer.launch({ headless: false, devTools: false });
+    const page = await browser.newPage();
+    await page.bringToFront();
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3683.103 Safari/537.36');
+    await page.setExtraHTTPHeaders({
+      'Accept-Language': 'en-US,en;q=0.9'
     });
+
+    await page.goto('https://www.google.es');
   });
 
   test('Iniciando sesión', ({ given, when, then }) => {
     
     given('Soy un usuario intentando iniciar sesión', async () => {
-      await expect(page.title()).resolves.toMatch('Solid App');
-      const element = await page.$("h1");
-      const text = await page.evaluate(element => element.textContent, element);
-      expect(text).toMatch("Hi! Welcome to Solid.");
+
+
     });
 
     when('relleno el formulario y lo envío', async () => {
 
-      await page.waitForSelector(INPUT_SELECTOR);
-      const input = await page.$(INPUT_SELECTOR);
-      input.press(ENTER_EVENT, "https://UO264046.solid.community/profile/card#me");
-      await page.$eval('button', el => { console.log(el); el.click();});
-      
+        //document.getElementsByClassName('sc-gzVnrw isbeaB ids-links')[0].value = 'user';
+        //document.getElementById('password').value = 'pass';
+        //document.getElementById('submit').click()
+        //document.getElementsByName("idp")[0].value = 'hola'
+        //document.querySelectorAll('button')[0].click();
+
+       // await page.focus('input');
+        
+        const element = page.waitForSelector('#realbox');
+        
+        //await page.evaluate(element => { element.setAttribute('value', 1234); }, element);
+
+        await page.focus('#realbox');
+        
+        //await page.type('#fakebox-input', 'aaaaaa');
+
+        //await page.type('123456');
+
+        await delay(9000);
+        
+        page.waitForSelector('button').then(async () => await page.click('submit'))
+
+        
+
     });
 
     then('nos redirige al la página de Solid', async () => {
-      const element = await page.$("h1");
-      const text = await page.evaluate(element => element.textContent, element);
+      //const element = await page.$("h1");
+      //const text = await page.evaluate(element => element.textContent, element);
       //expect(text).toMatch("Login");
     });
   });
