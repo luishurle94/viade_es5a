b7007f79577fcf1a587dfcdd3cc54f841aaf3629
Adri-San <adrianvadrianv@outlook.com>
2020-03-31 14:48:04 +0200

    Testing feature - Funcionalidad de rellenar formulario (en pruebas)

--- feature/steps/login.steps.js
+++ feature/steps/login.steps.js
@@ -2,28 +2,13 @@ import 'jest'
 import { defineFeature, loadFeature } from 'jest-cucumber';
 
 const feature = loadFeature('./feature/features/login.feature');
-const puppeteer = require('puppeteer');
-
-function delay(time) {
-  return new Promise(function(resolve) { 
-      setTimeout(resolve, time)
-  });
-}
+const puppeteer = require('puppeteer')
+const screenshot = 'amazon_nyan_cat_pullover.png'
 
 defineFeature(feature, test => {
 
   beforeEach(async () => {
-
-    const browser = await puppeteer.launch({ headless: false, devTools: false });
-    const page = await browser.newPage();
-    await page.bringToFront();
-    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3683.103 Safari/537.36');
-    await page.setExtraHTTPHeaders({
-      'Accept-Language': 'en-US,en;q=0.9'
-    });
-
-    await page.goto('https://www.google.es');
-  });
+});
 
   test('Iniciando sesión', ({ given, when, then }) => {
     
@@ -34,29 +19,67 @@ defineFeature(feature, test => {
 
     when('relleno el formulario y lo envío', async () => {
 
-        //document.getElementsByClassName('sc-gzVnrw isbeaB ids-links')[0].value = 'user';
-        //document.getElementById('password').value = 'pass';
-        //document.getElementById('submit').click()
-        //document.getElementsByName("idp")[0].value = 'hola'
-        //document.querySelectorAll('button')[0].click();
-
-       // await page.focus('input');
+      (async () => {
+        const browser = await puppeteer.launch({
+          headless: false
+        })
+        const page = await browser.newPage()
+        await page.goto("https://www.instagram.com/accounts/login/?source=auth_switcher", {
+          waitUntil: 'networkidle2'
+        });
+      
+        //email
+        await page.waitForSelector("[name='username']");
+        // await page.click("[name='username']");
+        await page.type("[name='username']", "Adri");
+      
+        //password
+        await page.keyboard.down("Tab");
+        //uncomment the following if you want the passwor dto be visible
+        // page.$eval("._2hvTZ.pexuQ.zyHYP[type='password']", (el) => el.setAttribute("type", "text"));
+        await page.keyboard.type("Contrasennna");
+      
+        //the selector of the "Login" button
+        // await page.click("._0mzm-.sqdOP.L3NKy>.Igw0E.IwRSH.eGOV_._4EzTm");
         
-        const element = page.waitForSelector('#realbox');
-        
-        //await page.evaluate(element => { element.setAttribute('value', 1234); }, element);
-
-        await page.focus('#realbox');
-        
-        //await page.type('#fakebox-input', 'aaaaaa');
-
-        //await page.type('123456');
-
-        await delay(9000);
-        
-        page.waitForSelector('button').then(async () => await page.click('submit'))
-
-        
+        //we find the Login btn using the innerText comparison because the selector used for the btn might be unstable
+        await page.evaluate(() => {
+          let btns = [...document.querySelector(".HmktE").querySelectorAll("button")];
+          btns.forEach(function (btn) {
+            if (btn.innerText == "Iniciar sesión")
+              btn.click();
+          });
+        });
+      
+        //Optional
+        //check if the element asking to download the app arises
+        // try {
+        // 	await loginPage.waitForSelector("._3m3RQ._7XMpj",{
+        // 		timeout:3000
+        // 	});
+        // 	await loginPage.click("._3m3RQ._7XMpj");
+        // } catch (err) {
+      
+        // }
+      
+        //Optional
+        //check if the app asks for notifications
+        // try {
+        // 	await loginPage.waitForSelector(".aOOlW.HoLwm",{
+        // 		timeout:5000
+        // 	});
+        // 	await loginPage.click(".aOOlW.HoLwm");
+        // } catch (err) {
+      
+        // }
+      
+        await page.waitForSelector(".glyphsSpriteMobile_nav_type_logo");
+      
+        await page.screenshot({ path: screenshot });
+      
+        browser.close()
+        console.log('See screenshot: ' + screenshot)
+      })()
 
     });
 
